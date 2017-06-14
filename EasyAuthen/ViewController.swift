796b87def7a330393d2a8355f5760c5cb2c91a5f
sutisa <a_sutisa@snru.ac.th>
2017-06-14 10:30:50 +0700

    checkUser OK

--- EasyAuthen/ViewController.swift
+++ EasyAuthen/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var userTextField: UIImageView!
+    //Implicit
+    var strUser: String?
+    var strPassword: String?
     
     
-    @IBOutlet weak var passwordTextField: UIImageView!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     
     
     
@@ -22,21 +31,66 @@ class ViewController: UIViewController {
     
     
     
-    @IBAction func loginButtom(_ sender: Any) {
-    }
-    
+    @IBAction func loginButtom2(_ sender: Any) {
+        
+        //get value form Textfield
+        strUser = usernameTextField.text
+        strPassword = passwordTextField.text
+        
+        //show user and pass on console
+        print("user ==>  \(strUser!)")
+        print("password ==> \(strPassword!)")
 
+        //find count string
+        let intUser = strUser?.characters.count
+        let intPass = strPassword?.characters.count
+        
+        print("intUser ==> \(intUser!)")
+        print("intPass ==> \(intPass!)")
+        
+        //call checkSpace
+        if checkSpace(myString: strUser!) {
+            print("user OK")
+        } else {
+        
+        print("blank user")
+        
+        }
+        
+        
+        
+        
+    } //buttom
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        
+        
+    }//main method
 
+    func checkSpace (myString: String) -> Bool {
+
+        let intString = myString.characters.count
+        var result: Bool = true
+ 
+        if intString == 0 {
+            //have space
+            result = false
+        }
+        
+        
+        return result
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }// didRecieve
 
 
-}
+} //mainclass
 
